22ace6bd1bb3ab68c90411ade61ad46799b76a97
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2026-04-04 00:15:58 +0700

    add

--- backend/controller/report/pdf.go
+++ backend/controller/report/pdf.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	fixedCaptureURL = "http://localhost:5173/capture" // เปลี่ยนด้วยใน docker-compose.yml ถ้าจำเป็น
+	fixedCaptureURL = "https://openvaswebv1.vercel.app/capture" // เปลี่ยนด้วยใน docker-compose.yml ถ้าจำเป็น
 	fixedPublicBase  = "https://postdiphtherial-unperishable-carolyn.ngrok-free.dev"
 	fixedReportsDir  = "./tmp/reports"
 	defaultPDFPrefix = "report_capture"
