a0311591f70190f07a1c8bc34739d3e6c5c6c9ac
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2026-04-17 12:23:35 +0700

    add

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const apiUrl = "http://localhost:9000"; // เปลี่ยนเป็น URL ของ API ของคุณ http://localhost:9000 and https://bayleigh-fendered-uncaustically.ngrok-free.dev
-
+export const apiUrl = "https://bayleigh-fendered-uncaustically.ngrok-free.dev"; // เปลี่ยนเป็น URL ของ API ของคุณ http://localhost:9000 and https://bayleigh-fendered-uncaustically.ngrok-free.dev
 export const defaultHeaders = {
   "Content-Type": "application/json",
   "ngrok-skip-browser-warning": "true",
